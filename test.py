c80f2c47fa3c03fc021c8f5e67284db91bcfac77
Tinttu7 <tinttu7@yahoo.com>
2017-10-07 12:39:11 +0300

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+print("hello world")
+
+name= "Stina"
+city="Porvoo"
+print("Hello my name is "+ name)
+print(name+" lives in "+city)
+Adress= " Huvituksentie 23 06150 " +city
+print("hello my name is "+name+" I live in "+city+". My address is"+Adress)
+födelseår=[1976, 1978, 1983, 1946, 1949]
+print(födelseår)
+födelseår.sort()
+print(födelseår)
